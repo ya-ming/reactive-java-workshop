98c7732c60a1729c0df31a292466151effebf070
YaMing <wuyaming@gmail.com>
2023-11-12 23:31:37 -0600

    execrise 1

---
+++ README.md
@@ -0,0 +1,20 @@
+# Reactive Programming
+
+## Why?
+* Compare to concurrency APIs (Future and CompletatbleFuture)
+    * Much simpler than the manual concurrent way
+    * Few reusable flexible functions
+    * Combine and reuse in powerful ways
+
+## Stream
+* Represent a sequence of data
+* Focus on computations
+* vs collections which focus on storage
+* Internal iteration
+
+* Operators
+    * map
+    * filter
+    * flatMap
+    * findFirst
+    * ... and many more
--- src/main/java/io/javabrains/reactiveworkshop/Exercise1.java
+++ src/main/java/io/javabrains/reactiveworkshop/Exercise1.java
@@ -7,24 +7,53 @@ public class Exercise1 {
         // Use StreamSources.intNumbersStream() and StreamSources.userStream()
 
         // Print all numbers in the intNumbersStream stream
-        // TODO: Write code here
+        StreamSources.intNumbersStream().forEach(number -> System.out.println(number));
+        System.out.println("");
 
         // Print numbers from intNumbersStream that are less than 5
-        // TODO: Write code here
+        StreamSources.intNumbersStream()
+                .filter(number -> number < 5)
+                .forEach(number -> System.out.println(number));
+        System.out.println("");
 
         // Print the second and third numbers in intNumbersStream that's greater than 5
-        // TODO: Write code here
+        StreamSources.intNumbersStream()
+                .filter(number -> number > 5)
+                .skip(1)
+                .limit(2)
+                .forEach(number -> System.out.println(number));
+        System.out.println("");
 
-        //  Print the first number in intNumbersStream that's greater than 5.
-        //  If nothing is found, print -1
-        // TODO: Write code here
+        // Print the first number in intNumbersStream that's greater than 5.
+        // If nothing is found, print -1
 
+        Integer value = StreamSources.intNumbersStream()
+                .filter(number -> number > 5)
+                .findFirst()
+                .orElse(-1);
+        System.out.println(value);
+        System.out.println("");
+                
         // Print first names of all users in userStream
-        // TODO: Write code here
+        StreamSources.userStream()
+                .forEach(user -> System.out.println(user.getFirstName()));
+        System.out.println("");
+        StreamSources.userStream()
+                .map(user -> user.getFirstName())
+                .forEach(userName -> System.out.println(userName));
+        System.out.println("");
 
         // Print first names in userStream for users that have IDs from number stream
-        // TODO: Write code here
+        StreamSources.userStream()
+                .filter(u -> StreamSources.intNumbersStream().anyMatch(i -> i == u.getId()))
+                .forEach(u -> System.out.println(u.getFirstName()));
+        System.out.println("");
 
+        StreamSources.intNumbersStream()
+                .flatMap(id -> StreamSources.userStream().filter(user -> user.getId() == id))
+                .map(user -> user.getFirstName())
+                .forEach(userName -> System.out.println(userName));
+        System.out.println("");
     }
 
 }
